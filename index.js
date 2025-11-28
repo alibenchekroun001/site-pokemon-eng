ad8db132aeb0e65077064c73a1f7ba58ae2710f6
alibenchekroun001 <a.benchekroun@esisa.ac.ma>
2025-11-28 20:26:39 +0100

    update

--- index.js
+++ index.js
@@ -2,29 +2,29 @@
 
 // ---------- Données ----------
 const TYPES = [
-  'Normal','Fire','Water','Electric','Grass','Ice','Fighting','Poison','Ground','Flying',
+  'Normal','Grass','Fire','Water','Electric','Ice','Fighting','Poison','Ground','Flying',
   'Psychic','Bug','Rock','Ghost','Dragon','Dark','Steel','Fairy'
 ];
 
 const TYPE_CHART = {
-  Normal: { Rock:0.5, Ghost:0, Steel:0.5 },
-  Fire: { Fire:0.5, Water:0.5, Grass:2, Ice:2, Bug:2, Rock:0.5, Dragon:0.5, Steel:2 },
-  Water: { Fire:2, Water:0.5, Grass:0.5, Ground:2, Rock:2, Dragon:0.5 },
-  Electric: { Water:2, Electric:0.5, Grass:0.5, Ground:0, Flying:2, Dragon:0.5 },
-  Grass: { Fire:0.5, Water:2, Grass:0.5, Poison:0.5, Ground:2, Flying:0.5, Bug:0.5, Rock:2, Dragon:0.5, Steel:0.5 },
-  Ice: { Fire:0.5, Water:0.5, Grass:2, Ground:2, Flying:2, Dragon:2, Steel:0.5, Ice:0.5 },
-  Fighting: { Normal:2, Ice:2, Rock:2, Dark:2, Steel:2, Poison:0.5, Flying:0.5, Psychic:0.5, Bug:0.5, Fairy:0.5, Ghost:0 },
-  Poison: { Grass:2, Poison:0.5, Ground:0.5, Rock:0.5, Ghost:0.5, Steel:0, Fairy:2 },
-  Ground: { Fire:2, Electric:2, Grass:0.5, Poison:2, Flying:0, Bug:0.5, Rock:2, Steel:2 },
-  Flying: { Electric:0.5, Grass:2, Fighting:2, Bug:2, Rock:0.5, Steel:0.5 },
-  Psychic: { Fighting:2, Poison:2, Psychic:0.5, Dark:0, Steel:0.5 },
-  Bug: { Fire:0.5, Grass:2, Fighting:0.5, Poison:0.5, Flying:0.5, Psychic:2, Ghost:0.5, Dark:2, Steel:0.5, Fairy:0.5 },
-  Rock: { Fire:2, Ice:2, Fighting:0.5, Ground:0.5, Flying:2, Bug:2, Steel:0.5 },
-  Ghost: { Normal:0, Psychic:2, Ghost:2, Dark:0.5 },
-  Dragon: { Dragon:2, Steel:0.5, Fairy:0 },
-  Dark: { Fighting:0.5, Psychic:2, Ghost:2, Dark:0.5, Fairy:0.5 },
-  Steel: { Fire:0.5, Water:0.5, Electric:0.5, Ice:2, Rock:2, Fairy:2, Steel:0.5 },
-  Fairy: { Fire:0.5, Fighting:2, Poison:0.5, Dragon:2, Dark:2, Steel:0.5 }
+  Normal: { Rock:0.5, Steel:0.5, Ghost:0 },
+  Grass: { Water:2, Ground:2, Rock:2, Grass:0.5, Fire:0.5, Poison:0.5, Flying:0.5, Bug:0.5, Dragon:0.5, Steel:0.5 },
+  Fire: { Grass:2, Ice:2, Bug:2, Steel:2, Fire:0.5, Water:0.5, Rock:0.5, Dragon:0.5 },
+  Water: { Fire:2, Ground:2, Rock:2, Grass:0.5, Water:0.5, Dragon:0.5 },
+  Electric: { Water:2, Flying:2, Grass:0.5, Electric:0.5, Dragon:0.5, Ground:0 },
+  Ice: { Grass:2, Ground:2, Flying:2, Dragon:2, Fire:0.5, Water:0.5, Ice:0.5, Steel:0.5 },
+  Fighting: { Normal:2, Ice:2, Rock:2, Dark:2, Steel:2, Poison:0.5, Flying:0.5, Psychic:0.5, Bug:0.5, Ghost:0 },
+  Poison: { Grass:2, Poison:0.5, Ground:0.5, Rock:0.5, Ghost:0.5, Steel:0 },
+  Ground: { Fire:2, Electric:2, Poison:2, Rock:2, Steel:2, Grass:0.5, Bug:0.5, Flying:0 },
+  Flying: { Grass:2, Fighting:2, Bug:2, Electric:0.5, Rock:0.5, Steel:0.5 },
+  Psychic: { Fighting:2, Poison:2, Psychic:0.5, Steel:0.5, Dark:0 },
+  Bug: { Grass:2, Psychic:2, Dark:2, Fire:0.5, Fighting:0.5, Poison:0.5, Flying:0.5, Ghost:0.5, Steel:0.5 },
+  Rock: { Ice:2, Flying:2, Bug:2, Fighting:0.5, Ground:0.5, Steel:0.5 },
+  Ghost: { Psychic:2, Ghost:2, Dark:0.5, Steel:0.5, Normal:0 },
+  Dragon: { Dragon:2, Steel:0.5 },
+  Dark: { Psychic:2, Ghost:2, Fighting:0.5, Dark:0.5, Steel:0.5 },
+  Steel: { Ice:2, Rock:2, Grass:0.5, Fire:0.5, Electric:0.5, Steel:0.5 },
+  Fairy: {  }
 };
 
 // ---------- Utilitaires ----------
